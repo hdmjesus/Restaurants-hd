6b0cdd51970da36e11f3760033aa64cc9906e209
Jesus Hernandez <hdmjesus@gmail.com>
2020-12-15 16:18:42 -0400

    Footer maquetado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <section>
-      <hr></hr>
-      <p>Jesus Hernandez</p> <span>@Animalia.do</span>
+    <section className='h-16 p-4 text-center flex flex-col'>
+      <hr className='w-1/2 m-auto'></hr>
+      <p className='text-sm inline-block mt-4 text-fontB'>
+        {''}
+        Jesus Hernandez<span>@Animalia.do</span>{' '}
+      </p>
     </section>
   );
 };
